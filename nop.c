c63a4f32a657e5d978f0dff9b5ac86259a32952e
juan20carlos <juancarlos14089@gmail.com>
2022-05-16 03:04:26 +0000

    version i

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn´t do anything
+ *
+ * @stack: double linked list
+ * @line_number: Number to add (not used)
+ * Return: Nothing
+ */
+
+void nop(stack_t **stack __attribute__((unused)),
+		unsigned int line_number __attribute__((unused)))
+{
+}
