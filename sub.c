0995f4bba986426c7ed48bf6dbb11eb7b3d420d2
juan20carlos <juancarlos14089@gmail.com>
2022-05-16 03:26:22 +0000

    version 1

---
+++ sub.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * sub - sub the first tow values at the top of a double linked list
+ *
+ * @stack: double linked list
+ * @line_number: Number to add (not used)
+ * Return: Nothing
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = NULL, *second = NULL;
+	size_t len = 0;
+	int add_number = 0;
+
+	len = stack_len(*stack);
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		status = EXIT_FAILURE;
+		return;
+	}
+	first = *stack;
+	second = first->next;
+	add_number = second->n - first->n;
+	second->n = add_number;
+	pop(stack, line_number);
+	second->prev = NULL;
+	*stack = second;
+}
